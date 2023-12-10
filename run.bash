e08c96214ca67a654d8c6fe0fe377cb83415d4bd
Jean A. Flaherty <s1292010@u-aizu.ac.jp>
2023-12-10 16:34:28 +0900

    update script

--- run.bash
+++ run.bash
@@ -3,6 +3,16 @@ for DIR in "$DATASETS"/*; do
     if [ -d "$DIR" ]; then
         SCENE=$(basename $DIR)
         echo "Training on: $SCENE"
-        python scripts/run.py --scene $DIR --save_snapshot out/$SCENE/default/snapshot.pt --test_transforms ~/Datasets/NeRF/blender/$SCENE/transforms_test.json
+
+        # base config
+        CONFIG="configs/nerf/base.json"
+        EXPERIMENT_NAME="base"
+        python scripts/run.py --network $CONFIG --save_snapshot out/$SCENE/default/snapshot.pt --scene $DIR --test_transforms ~/Datasets/NeRF/blender/$SCENE/transforms_test.json
+
+        # base_1layer config
+        CONFIG="configs/nerf/base_1layer.json"
+        EXPERIMENT_NAME="base_1layer"
+        python scripts/run.py --network $CONFIG --save_snapshot out/$SCENE/default/snapshot.pt --scene $DIR --test_transforms ~/Datasets/NeRF/blender/$SCENE/transforms_test.json
+
     fi
 done
