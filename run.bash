5d6cb4e983e137b9b0be5c6fc447625d8cd82d7e
Jean A. Flaherty <s1292010@u-aizu.ac.jp>
2023-12-10 16:38:44 +0900

    update script

--- run.bash
+++ run.bash
@@ -7,12 +7,12 @@ for DIR in "$DATASETS"/*; do
         # base config
         CONFIG="configs/nerf/base.json"
         EXPERIMENT_NAME="base"
-        python scripts/run.py --network $CONFIG --save_snapshot out/$SCENE/default/snapshot.pt --scene $DIR --test_transforms ~/Datasets/NeRF/blender/$SCENE/transforms_test.json
+        python scripts/run.py --network $CONFIG --save_snapshot out/$SCENE/$EXPERIMENT_NAME/snapshot.pt --scene $DIR --test_transforms ~/Datasets/NeRF/blender/$SCENE/transforms_test.json
 
         # base_1layer config
         CONFIG="configs/nerf/base_1layer.json"
         EXPERIMENT_NAME="base_1layer"
-        python scripts/run.py --network $CONFIG --save_snapshot out/$SCENE/default/snapshot.pt --scene $DIR --test_transforms ~/Datasets/NeRF/blender/$SCENE/transforms_test.json
+        python scripts/run.py --network $CONFIG --save_snapshot out/$SCENE/$EXPERIMENT_NAME/snapshot.pt --scene $DIR --test_transforms ~/Datasets/NeRF/blender/$SCENE/transforms_test.json
 
     fi
 done
